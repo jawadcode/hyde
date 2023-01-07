31484a62d72fc56fa44bd1b73515f264aaee7648
Jawad Ahmed <jawad.w.ahmed@gmail.com>
2023-01-07 19:26:34 +0000

    minor changes

--- src/posts/frontmatter.rs
+++ src/posts/frontmatter.rs
@@ -3,18 +3,18 @@ use std::path::Path;
 use chrono::{DateTime, FixedOffset};
 use serde::Deserialize;
 
-use super::ParseError;
+use super::post::ParseError;
 
 #[derive(Debug, Clone, Deserialize, PartialEq)]
-pub struct Frontmatter<'src> {
-    pub title: &'src str,
+pub struct Frontmatter {
+    pub title: String,
     pub datetime: DateTime<FixedOffset>,
-    pub tags: Vec<&'src str>,
+    pub tags: Vec<String>,
 }
 
-impl<'src> Frontmatter<'src> {
+impl Frontmatter {
     /// Parse the `front_matter` as YAML
-    pub fn from_str(front_matter: &'src str, path: &Path) -> Result<Self, ParseError> {
+    pub(super) fn from_str(front_matter: &str, path: &Path) -> Result<Self, ParseError> {
         serde_yaml::from_str(front_matter).map_err(|err| ParseError {
             path: path.to_path_buf(),
             info: err.to_string(),
--- src/posts/post.rs
+++ src/posts/post.rs
@@ -1,22 +1,21 @@
-use std::path::{Path, PathBuf};
+use std::{
+    fmt::{self, Display},
+    path::PathBuf,
+};
 
-use pulldown_cmark::{html, Options, Parser};
-
-use self::frontmatter::Frontmatter;
-
-pub mod frontmatter;
+use super::{content::parse_content, frontmatter::Frontmatter};
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Post<'src> {
+pub struct Post {
     /// The path of the source file
     pub path: PathBuf,
     /// The frontmatter for this post, contains metadata
-    pub front_matter: Frontmatter<'src>,
+    pub front_matter: Frontmatter,
     /// The main content of the post, rendered as html
     pub content: String,
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct ParseError {
     /// The path of the file in which the error occurred
     pub path: PathBuf,
@@ -24,25 +23,35 @@ pub struct ParseError {
     pub info: String,
 }
 
-impl<'src> Post<'src> {
-    fn from_str(source: &'src str, path: &Path) -> Result<Self, ParseError> {
+impl Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}\nFailed to parse '{}'",
+            self.info,
+            self.path.display()
+        )
+    }
+}
+
+impl Post {
+    /// Parse a post, made up of the frontmatter and content
+    pub fn from_str(source: &str, path: PathBuf) -> Result<Self, ParseError> {
         let mut sections = source.split("---");
         let front_matter_text = sections.nth(1).ok_or_else(|| ParseError {
-            path: path.to_path_buf(),
+            path: path.clone(),
             info: "Missing frontmatter".to_string(),
         })?;
-        let front_matter = Frontmatter::from_str(front_matter_text, path)?;
+        let front_matter = Frontmatter::from_str(front_matter_text, &path)?;
 
         let content_markdown = sections.next().ok_or_else(|| ParseError {
-            path: path.to_path_buf(),
+            path: path.clone(),
             info: "Missing frontmatter terminator".to_string(),
         })?;
-        let parser = Parser::new_ext(content_markdown, Options::empty());
-        let mut content = String::new();
-        html::push_html(&mut content, parser);
+        let content = parse_content(content_markdown);
 
         Ok(Self {
-            path: path.to_path_buf(),
+            path,
             front_matter,
             content,
         })
@@ -58,14 +67,14 @@ datetime: 2022-12-23T02:58:04.390Z
 tags:
 ---
 **egg**"#;
-    let path = Path::new("test").to_path_buf();
-    let test = Post::from_str(test, &path);
+    let path = PathBuf::from("test");
+    let test = Post::from_str(test, path.clone());
     assert_eq!(
         test,
         Ok(Post {
             path,
             front_matter: Frontmatter {
-                title: "My Favourite Recipe",
+                title: "My Favourite Recipe".to_string(),
                 datetime: DateTime::parse_from_rfc3339("2022-12-23T02:58:04.390Z").unwrap(),
                 tags: Vec::new()
             },
