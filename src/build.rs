3aeb3b7d4e0046a857773c516c89a8caa0a1a165
Jawad Ahmed <jawad.w.ahmed@gmail.com>
2023-09-14 00:29:52 +0100

    add full summary

--- src/build.rs
+++ src/build.rs
@@ -11,7 +11,11 @@ pub enum BuildError {}
 /// The [`Result`] of trying to build a Hyde project
 pub type BuildRes = std::result::Result<(), BuildError>;
 
-/// Build a Hyde project
+/// Build the Hyde project in `dir`
+///
+/// # Summary
+///
+/// Read from the `hyde.toml` config file, create the `static/` directory for the statically generated output, copy over the auxiliary theme files, and compile all of the posts in the `posts/` directory into it, using the `templates/` from the theme specified in the config.
 pub fn build_proj(dir: impl AsRef<Path>) -> BuildRes {
     let config_path = dir.as_ref().join("hyde.toml");
     todo!()
