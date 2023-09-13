cc2c9ae25db2f5da24a2093af95081d414849ae1
Jawad Ahmed <jawad.w.ahmed@gmail.com>
2023-09-13 23:29:22 +0100

    document public items

--- src/new.rs
+++ src/new.rs
@@ -1,3 +1,5 @@
+//! Creating a new project
+
 use std::{
     fs::{self, File},
     io::{self, Write},
@@ -7,8 +9,10 @@ use std::{
 use include_dir::include_dir;
 use snafu::Snafu;
 
+/// An error that occurred during the creation of a Hyde project, there is not much that can go wrong other than IO errors, so this just serves as a classification for them
 #[derive(Debug, Snafu)]
 pub enum CreateError {
+    /// A miscellaneous IO error, e.g. from trying to write to the config
     #[snafu(display("IO Error: {source}"))]
     Io { source: io::Error },
 
@@ -22,10 +26,12 @@ pub enum CreateError {
     ExtractTheme { source: io::Error },
 }
 
+/// The [`Result`] of trying to create a Hyde project
 pub type CreateRes = std::result::Result<(), CreateError>;
 
 static DEFAULT_THEME: include_dir::Dir = include_dir!("$CARGO_MANIFEST_DIR/default_theme");
 
+/// Create a new Hyde project with a name (passed to templates for generating the `<title>`) and optional description, including creating and writing the default config as well as extracting the embedded default theme into the project dir
 pub fn new_project(dir: impl AsRef<Path>, name: &str, desc: Option<&str>) -> CreateRes {
     let dir = dir.as_ref().join(name);
     fs::create_dir(&dir).map_err(|source| CreateError::ProjectDir { source })?;
